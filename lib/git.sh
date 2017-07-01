1f9f91433d93ee086566e005c90a7b248158b6fc
Yeongho Kim <yeonghoey@gmail.com>
2017-07-01 17:28:56 +0900

    Update

--- lib/git.sh
+++ lib/git.sh
@@ -6,6 +6,7 @@ clone-or-pull() {
   local repo="$1"
   local directory="$2"
   if [[ -d "$directory" ]]; then
+    # TODO: Validate repo
     git -C "$directory" pull
   else
     git clone "$repo" "$directory"
