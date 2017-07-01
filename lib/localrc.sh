0a456dad9b7bec4ef9bd5642529c819b4712e252
Yeongho Kim <yeonghoey@gmail.com>
2017-07-01 17:40:54 +0900

    Update

--- lib/localrc.sh
+++ lib/localrc.sh
@@ -2,10 +2,16 @@
 
 set -euo pipefail
 
+LOCALRC="$HOME/.localrc"
+
+if [[ ! -e "$LOCALRC" ]]; then
+  touch "$LOCALRC"
+fi
+
 append-localrc() {
   local line="$1"
   # Append only there is no exact matching line.
-  if grep --quiet --invert-match --fixed-strings "$line"; then
-    echo "$line" >> "$HOME/.localrc"
+  if ! grep --quiet --fixed-strings "$line" "$LOCALRC"; then
+    echo "$line" >> "$LOCALRC"
   fi
 }
