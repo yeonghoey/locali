edccecaf62b46daa7706932a4f1c3e69df7a8282
Yeongho Kim <yeonghoey@yeonghoey-imac.local>
2017-07-08 17:16:02 +0900

    Refactor brew

--- install/brew-install.bash
+++ install/brew-install.bash
@@ -3,35 +3,26 @@
 set -euo pipefail
 
 run_brew_install() {
-  # Declare LOCAL_BIN
-  LOCAL_BIN="${HOME}/.local/bin"
-  mkdir -p "${LOCAL_BIN}"
-
-  # Add LOCAL_BIN to PATH
-  if [[ ":${PATH}:" != *":${LOCAL_BIN}:"* ]]; then
-    PATH="${LOCAL_BIN}:${PATH}"
-  fi 
-  
-  brew update
-
-  brew install 'cmake'
-  brew install 'diff-so-fancy'
-  brew install 'fasd'
-  brew install 'fpp'
-  brew install 'gawk'
-  brew install 'git'
-  brew install 'git-extras'
-  brew install 'git-lfs'
-  brew install 'go'
-  brew install 'hub'
-  brew install 'imagemagick'
-  brew install 'ispell'
-  brew install 'node'
-  brew install 'readline'
-  brew install 'the_silver_searcher'
-  brew install 'tmux'
-  brew install 'wget'
-  brew install 'xz'
+  local brew="${HOME}/.local/bin/brew"
+  $brew update
+  $brew install 'cmake'
+  $brew install 'diff-so-fancy'
+  $brew install 'fasd'
+  $brew install 'fpp'
+  $brew install 'gawk'
+  $brew install 'git'
+  $brew install 'git-extras'
+  $brew install 'git-lfs'
+  $brew install 'go'
+  $brew install 'hub'
+  $brew install 'imagemagick'
+  $brew install 'ispell'
+  $brew install 'node'
+  $brew install 'readline'
+  $brew install 'the_silver_searcher'
+  $brew install 'tmux'
+  $brew install 'wget'
+  $brew install 'xz'
 }
 
 run_brew_install
