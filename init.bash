eeaa1866655d6b1d4e2867627582160afbdf88b2
Yeongho Kim <yeonghoey@gmail.com>
2017-07-10 03:45:51 +0900

    Refactor init.bash

--- init.bash
+++ init.bash
@@ -1,58 +1,56 @@
 #!/usr/bin/env bash
 {
-# Set LOCALISH_DEV to apply developing changes
-# before publishing it
-if [[ "${LOCALISH_DEV-}" ]]; then
-  unset LOCALISH_DEV
-  source init.bash
-else
-  export LOCALRC="${HOME}/.localrc"
-  touch "${LOCALRC}"
+# Declare base variables
+export LOCALRC="${HOME}/.localrc"
+touch "${LOCALRC}"
 
-  export LOCAL_ROOT="${HOME}/.local"
-  mkdir -p "${LOCAL_ROOT}"
+export LOCAL_ROOT="${HOME}/.local"
+mkdir -p "${LOCAL_ROOT}"
 
-  export LOCAL_BIN="${LOCAL_ROOT}/bin"
-  mkdir -p "${LOCAL_BIN}"
+export LOCAL_BIN="${LOCAL_ROOT}/bin"
+mkdir -p "${LOCAL_BIN}"
 
-  export LOCAL_REPO="${LOCAL_ROOT}/repo"
-  mkdir -p "${LOCAL_REPO}"
+export LOCAL_REPO="${LOCAL_ROOT}/repo"
+mkdir -p "${LOCAL_REPO}"
 
-  addpath() {
-    if [[ ":${PATH}:" != *":$1:"* ]]; then
-      PATH="$1:${PATH}"
-    fi
-  }
 
-  homepath() {
-    local path="$1"
-    local relpath="${path#"${HOME}"}"
-    echo "\${HOME}${relpath}"
-  }
+# Define helper functions
+addpath() {
+  if [[ ":${PATH}:" != *":$1:"* ]]; then
+    PATH="$1:${PATH}"
+  fi
+}
 
-  gitrepo() {
-    local url="$1"
-    local name="$(basename ${url} .git)"
-    if [[ "$#" == 1 ]]; then
-      local target="${LOCAL_REPO}/${name}"
-    elif [[ "$#" == 2 ]]; then
-      local prefix="$2"
-      local target="${LOCAL_REPO}/${prefix}/${name}"
-    fi
-    if [[ -e "${target}" ]]; then
-      git -C "${target}" pull
-    else
-      git clone "${url}" "${target}"
-    fi
-  }
+homepath() {
+  local path="$1"
+  local relpath="${path#"${HOME}"}"
+  echo "\${HOME}${relpath}"
+}
 
-  localrc() {
-    local content="$(cat -)"
-    if ! grep -q "${content}" "${LOCALRC}"; then
-      echo "$content" >> "${LOCALRC}"
-    fi
-  }
+gitrepo() {
+  local url="$1"
+  local name="$(basename ${url} .git)"
+  if [[ "$#" == 1 ]]; then
+    local target="${LOCAL_REPO}/${name}"
+  elif [[ "$#" == 2 ]]; then
+    local prefix="$2"
+    local target="${LOCAL_REPO}/${prefix}/${name}"
+  fi
+  if [[ -e "${target}" ]]; then
+    git -C "${target}" pull
+  else
+    git clone "${url}" "${target}"
+  fi
+}
 
-  addpath "${LOCAL_BIN}"
-fi
+localrc() {
+  local content="$(cat -)"
+  if ! grep -q "${content}" "${LOCALRC}"; then
+    echo "$content" >> "${LOCALRC}"
+  fi
+}
+
+
+# Initialize
+addpath "${LOCAL_BIN}"
 }
