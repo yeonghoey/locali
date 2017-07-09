32cf2b6cce2517a5d82be5af2c5ac29f6b79dce0
Yeongho Kim <yeonghoey@gmail.com>
2017-07-10 02:26:14 +0900

    Working on pyenv

--- init.bash
+++ init.bash
@@ -4,7 +4,12 @@
 git_repo() {
   local url="$1"
   local name="$(basename ${url} .git)"
-  local target="${LOCAL_REPO}/${name}"
+  if [[ "$#" == 1 ]]; then
+    local target="${LOCAL_REPO}/${name}"
+  elif [[ "$#" == 2 ]]; then
+    local prefix="$2"
+    local target="${LOCAL_REPO}/${prefix}/${name}"
+  fi
   if [[ -e "${target}" ]]; then
     git -C "${target}" pull
   else
@@ -18,11 +23,16 @@ add_path() {
   fi 
 }
 
+export LOCALRC="${HOME}/.localrc"
+localrc() {
+  local content="$(cat -)"
+  echo "$content"
+}
+
 export LOCAL_REPO="${HOME}/.local/repo"
 mkdir -p "${LOCAL_REPO}"
 
 export LOCAL_BIN="${HOME}/.local/bin"
 mkdir -p "${LOCAL_BIN}"
-
 add_path "${LOCAL_BIN}"
 }
