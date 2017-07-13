cbad7e89dcb73c9a98baa40627748f83812109f4
Yeongho Kim <yeonghoey@gmail.com>
2017-07-13 09:08:40 +0900

    Update

--- init.bash
+++ init.bash
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 {
+# Declare environment variables
+export LOCAL_ROOT="${HOME}/.local"
+export LOCAL_REPO="${LOCAL_ROOT}/repo"
+export LOCAL_BIN="${LOCAL_ROOT}/bin"
+export LOCALRC="${HOME}/.localrc"
+
+mkdir -p "${LOCAL_ROOT}"
+mkdir -p "${LOCAL_REPO}"
+mkdir -p "${LOCAL_BIN}"
+touch "${LOCALRC}"
 
 include() {
   if [[ "${LOCALISH:-}" ]]; then
@@ -17,17 +27,6 @@ require() {
   fi
 }
 
-export LOCAL_ROOT="${HOME}/.local"
-export LOCAL_REPO="${LOCAL_ROOT}/repo"
-export LOCAL_BIN="${LOCAL_ROOT}/bin"
-export LOCALRC="${HOME}/.localrc"
-
-mkdir -p "${LOCAL_ROOT}"
-mkdir -p "${LOCAL_REPO}"
-mkdir -p "${LOCAL_BIN}"
-touch "${LOCALRC}"
-
-export PATH="${LOCAL_BIN}:${PATH}"
 localrc() {
   local content="$(cat -)"
   if ! grep -q "${content}" "${LOCALRC}"; then
@@ -35,6 +34,8 @@ localrc() {
   fi
 }
 
+# Add to PATH
+export PATH="${LOCAL_BIN}:${PATH}"
 localrc <<- EOF
   export PATH="$(homepath ${LOCAL_BIN}):\${PATH}"
 EOF
