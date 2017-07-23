3d8575f942b274e551d671d91e0f3fad476b145b
Yeongho Kim <yeonghoey@yeonghoey-imac.local>
2017-07-23 10:20:31 +0900

    Update

--- setup/init.bash
+++ setup/init.bash
@@ -3,33 +3,36 @@
 export LOCAL_ROOT="${HOME}/.local"
 export LOCAL_REPO="${LOCAL_ROOT}/repo"
 export LOCAL_BIN="${LOCAL_ROOT}/bin"
-export LOCAL_RC="${HOME}/.localrc"
+export LOCALRC="${HOME}/.localrc"
 
 mkdir -p "${LOCAL_ROOT}"
 mkdir -p "${LOCAL_REPO}"
 mkdir -p "${LOCAL_BIN}"
-touch "${LOCAL_RC}"
+touch "${LOCALRC}"
 
-local-rc() {
+localrc() {
   local label="$1"
-  local content="$(cat -)"
-
-  # Replace '/user/<username>/.local/*' with '${HOME}/.local/*'
-  local relpath="${LOCAL_ROOT#"${HOME}"}"
-  local sedexp="s:${LOCAL_ROOT}:\${HOME}${relpath}:g"
-  content="$(echo $content | sed "${sedexp}")"
+  local content="$(homepath "$(cat -)")"
 
   # Put label
   content="$(echo -e "# ${label}\n${content}")"
 
   # Append content only when the content is not literally matched
-  if ! grep -qF "${content}" "${LOCAL_RC}"; then
+  if ! grep -qF "${content}" "${LOCALRC}"; then
     # Put a blank line
-    echo -e "\n${content}" >> "${LOCAL_RC}"
+    echo -e "\n${content}" | tee -a "${LOCALRC}"
   fi
 }
 
-git-repo() {
+homepath() {
+  # Replace '/user/<username>/.local/*' with '${HOME}/.local/*'
+  local content="$1"
+  local relpath="${LOCAL_ROOT#"${HOME}"}"
+  local sedexp="s:${LOCAL_ROOT}:\${HOME}${relpath}:g"
+  echo "$content" | sed "${sedexp}"
+}
+
+gitrepo() {
   local url="$1"
   local name="$(basename ${url} .git)"
   if [[ "$#" == 1 ]]; then
@@ -62,6 +65,6 @@ bin-link() {
 export PATH="${LOCAL_BIN}:${PATH}"
 
 # Add to PATH in ~/.localrc for future use
-local-rc 'localish' << EOF
+localrc 'localish' << EOF
 export PATH="${LOCAL_BIN}:\${PATH}"
 EOF
