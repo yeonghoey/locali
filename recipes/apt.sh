668748d9abdd156de0d8f76517f113a6024f0e19
Yeongho Kim <yeonghoey@gmail.com>
2017-08-09 12:34:11 +0900

    Update

--- recipes/apt.sh
+++ recipes/apt.sh
@@ -1,5 +1,4 @@
 ubuntu && {
-  use_sudo
   cat "${LOCALISH}/etc/apt-repositories.txt" | xargs sudo add-apt-repository
   sudo apt-get update
   cat "${LOCALISH}/etc/apt-packages.txt" | xargs sudo apt-get install -y
