857fd105dce1166e1a564727ec7229d9f4617870
Yeongho Kim <yeonghoey@gmail.com>
2017-08-11 20:45:11 +0900

    Rename recipes/preferences.sh to recipes/shell.sh

---
+++ recipes/shell.sh
@@ -0,0 +1,14 @@
+info "Set locale environment variables to 'en_US.UTF-8'"
+
+localrc 'shell' <<EOF
+export LANG='en_US.UTF-8'
+export LC_ALL='en_US.UTF-8'
+EOF
+
+# -------------------------------------------------------------------------------
+
+info "Use zsh"
+
+ZSH_PATH="$(command_path 'zsh')"
+require_content '/etc/shells' "${ZSH_PATH}"
+chsh -s "${ZSH_PATH}"
