c6c440c13c240fa710d57108b1cc007ad13efbe5
Yeongho Kim <yeonghoey@gmail.com>
2017-08-12 23:45:48 +0900

    Update for anki

---
+++ recipes/anki.sh
@@ -0,0 +1,13 @@
+# Add ANKI_MEDIA which anki scripts depend on to .localrc (macOS, Anki2)
+
+# ------------------------------------------------------------------------------
+
+require_macos
+
+# ------------------------------------------------------------------------------
+
+prompt 'Anki Prfile Name'
+
+localrc 'anki' <<EOF
+export ANKI_MEDIA="\$HOME/Library/Application Support/Anki2/$(answer)/collection.media"
+EOF
