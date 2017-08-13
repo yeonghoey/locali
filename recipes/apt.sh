d9747bae067f8e33e181c09fda1bcb815f1ca010
Yeongho Kim <yeonghoey@gmail.com>
2017-08-13 17:53:02 +0900

    Update apt

--- recipes/apt.sh
+++ recipes/apt.sh
@@ -1,6 +1,12 @@
-ubuntu && {
-  cat "${LOCALISH}/ingredients/apt-repositories.txt" | xargs sudo add-apt-repository
-  sudo apt-get update
-  cat "${LOCALISH}/ingredients/apt-packages.txt" | xargs sudo apt-get install -y
-  sudo apt autoremove
-}
+# Add apt repositories and install packages
+
+# ------------------------------------------------------------------------------
+
+require_ubuntu
+
+# ------------------------------------------------------------------------------
+
+sudo add-apt-repository "$(requirements "${LOCALISH}/apt/repositories.txt")"
+sudo apt-get update
+sudo apt-get install -y "$(requirements "${LOCALISH}/apt/packages.txt")"
+sudo apt autoremove
