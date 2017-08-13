c929648f5786dcea4e33336a3545be3018c170fb
Yeongho Kim <yeonghoey@gmail.com>
2017-08-13 16:01:41 +0900

    Refactor recipes/yeonghoey.sh

--- recipes/yeonghoey.sh
+++ recipes/yeonghoey.sh
@@ -1,5 +1,13 @@
-repo_git 'git@github.com:yeonghoey/notes.git'
-repo_sym 'notes' "$HOME/notes"
+# Personal repositories
 
-repo_git 'git@github.com:yeonghoey/org.git'
-repo_sym 'org' "$HOME/org"
+# ------------------------------------------------------------------------------
+
+repo_git 'git@github.com:yeonghoey/yeonghoey.git'
+repo_run 'yeonghoey/update.sh'
+repo_sym 'yeonghoey' "$HOME/yeonghoey"
+
+# ------------------------------------------------------------------------------
+
+repo_git 'git@github.com:yeonghoey/yeonghoey-org.git'
+repo_run 'yeonghoey-org/update.sh'
+repo_sym 'yeonghoey-org' "$HOME/yeonghoey-org"
