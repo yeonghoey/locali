204104a92253e2cf6346f5386072d7020cfcc412
Yeongho Kim <yeonghoey@gmail.com>
2017-08-13 16:27:44 +0900

    Add recipes/vim.sh

---
+++ recipes/vim.sh
@@ -0,0 +1,7 @@
+# Init Vim (dotfiles required)
+
+# ------------------------------------------------------------------------------
+
+repo_git 'https://github.com/VundleVim/Vundle.vim.git'
+repo_sym 'Vundle.vim' "$HOME/.vim/bundle/Vundle.vim"
+vim +PluginInstall +qall
