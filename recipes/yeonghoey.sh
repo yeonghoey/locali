166e9f6ac0351ac52e48d80b208ef7b820260bce
Yeongho Kim <yeonghoey@gmail.com>
2017-08-20 15:35:57 +0900

    Update yeonghoey.sh

--- recipes/yeonghoey.sh
+++ recipes/yeonghoey.sh
@@ -8,12 +8,13 @@ git -C "${LOCALISH}" remote set-url \
 
 # ------------------------------------------------------------------------------
 
-repo_git 'git@github.com:yeonghoey/yeonghoey.git'
-repo_run 'yeonghoey/update.sh'
+GIT_LFS_SKIP_SMUDGE=1 \
+  repo_git 'git@github.com:yeonghoey/yeonghoey.git'
 repo_sym 'yeonghoey' "$HOME/yeonghoey"
+repo_run 'yeonghoey/update.sh'
 
 # ------------------------------------------------------------------------------
 
 repo_git 'git@github.com:yeonghoey/yeonghoey-org.git'
-repo_run 'yeonghoey-org/update.sh'
 repo_sym 'yeonghoey-org' "$HOME/yeonghoey-org"
+repo_run 'yeonghoey-org/update.sh'
