08efc90279a01467e015e9a4bb1493947740c5a2
Yeongho Kim <yeonghoey@gmail.com>
2017-08-20 10:13:46 +0900

    Add tmux recipe

--- recipes/dotfiles.sh
+++ recipes/dotfiles.sh
@@ -21,11 +21,5 @@ stow_localish "dotfiles" "${HOME}"
 # EOF
 # }
 
-# init_tpm() {
-#   repo_git 'https://github.com/tmux-plugins/tpm'
-#   repo_sym 'tpm' "$HOME/.tmux/plugins/tpm"
-# }
-
 # stow_dotfiles        || return 1
 # init_gitconfig_local || return 1
-# init_tpm             || return 1
---
+++ recipes/tmux.sh
@@ -0,0 +1,2 @@
+repo_git 'https://github.com/tmux-plugins/tpm'
+repo_sym 'tpm' "$HOME/.tmux/plugins/tpm"
