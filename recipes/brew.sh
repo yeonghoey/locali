40fc7c62c59b7ea89f905ad26d5892a58cb8cee8
Yeongho Kim <yeonghoey@gmail.com>
2017-08-20 14:46:15 +0900

    Working on github

--- recipes/brew.sh
+++ recipes/brew.sh
@@ -1,4 +1,4 @@
-require_macos
+on_macos
 
 repo_git 'https://github.com/Homebrew/brew.git'
 repo_bin 'brew/bin/brew'
---
+++ recipes/github.sh
@@ -0,0 +1,23 @@
+# Generate '.ssh/id_rsa'
+ID_RSA="${HOME}/.ssh/id_rsa"
+
+if [[ ! -f "${ID_RSA}" ]]; then
+
+  prompt 'GitHub email'
+  GITHUB_EMAIL="$(answer)"
+
+  # Pass prompts
+  echo | ssh-keygen -t rsa -b 4096 -C "${GITHUB_EMAIL}" -P ''
+  #  |                                                   +- no passphrase
+  #  +- pass prompt.
+  #     use only the default file path('.ssh/id_rsa')
+
+  eval "$(ssh-agent -s)"
+  ssh-add "${ID_RSA}"
+
+fi
+
+info 'Paste following public key on GitHub SSH menu'
+echo '--------------------------------------------------------------------------------'
+cat "${ID_RSA}.pub"
+echo '--------------------------------------------------------------------------------'
