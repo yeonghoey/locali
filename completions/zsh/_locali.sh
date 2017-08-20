1549e9ece5e9364fd790703165bc711916a74854
Yeongho Kim <yeonghoey@gmail.com>
2017-08-21 01:59:37 +0900

    Update completion of locali.sh

--- completions/zsh/_locali.sh
+++ completions/zsh/_locali.sh
@@ -20,26 +20,17 @@ readonly LOCALISH="$(realdir "$(command -v 'locali.sh')")"
 local -a recipes
 #      └─ array
 
-local recipe_name=''
-local recipe_desc=''
+local recipe_name
+local recipe_desc
 
 recipes=()
 for recipe in $LOCALISH/recipes/*.sh; do
   recipe_name="$(basename "${recipe%.*}")"
   recipe_desc="$(head -n 1 "$recipe")"
 
-  if [[ "${recipe_desc}" == "# "* ]]; then
-    recipe_desc="${recipe_desc:2}"
-  else
-    recipe_desc=""
-  fi
-
   recipes+=("${recipe_name}:${recipe_desc}")
 done
 
 # ------------------------------------------------------------------------------
-
-# NOTE: To stop grouping recipes with empty descriptions
-zstyle ':completion:*:*:locali.sh:*' list-grouped false
 
 _describe 'recipes' recipes
