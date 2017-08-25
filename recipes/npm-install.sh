6e47727f27918bc56458216cd0ce9169eb40c0d2
Yeongho Kim <yeonghoey@gmail.com>
2017-08-26 08:37:35 +0900

    Add nodemon to npm-install

--- recipes/npm-install.sh
+++ recipes/npm-install.sh
@@ -1,5 +1,6 @@
 # install npm global pacakges
 
-sudo npm install -g tern
-sudo npm install -g js-beautify
 sudo npm install -g eslint
+sudo npm install -g js-beautify
+sudo npm install -g nodemon
+sudo npm install -g tern
