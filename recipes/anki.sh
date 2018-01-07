241c044496847c20523249857bed37bd1047ab44
Yeongho Kim <yeonghoey@gmail.com>
2018-01-08 04:15:49 +0900

    Update anki

--- recipes/anki.sh
+++ recipes/anki.sh
@@ -11,3 +11,7 @@ prompt 'Anki Prfile Name'
 localrc 'anki' <<EOF
 export ANKI_MEDIA="\$HOME/Library/Application Support/Anki2/$(answer)/collection.media"
 EOF
+
+repo_git 'https://github.com/yeonghoey/ankia.git'
+repo_run 'ankia/install.sh'
+repo_bin 'ankia/ankia'
