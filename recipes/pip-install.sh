0d47438f892f6a2e9a4fbc07d8ea147ade620f05
yeonghoey <yeonghoey@gmail.com>
2018-04-11 17:53:34 +0900

    Refactor around pipenv

--- recipes/pip-install.sh
+++ recipes/pip-install.sh
@@ -6,14 +6,11 @@ info "Use '$(pyenv which 'pip')'"
 
 pip install -r <(cat <<EOF
 
-click==6.7
-cookiecutter==1.6.0
-hew>=0.0.3
-pipenv==10.1.0
-pyperclip==1.6.0
-pytube==8.0.2
-mkup==0.0.1
-requests==2.18.3
+click>=6.7
+cookiecutter>=1.6.0
+pyperclip>=1.6.0
+pytube>=8.0.2
+requests>=2.18.3
 
 EOF
 )
--- recipes/pyenv-install.sh
+++ recipes/pyenv-install.sh
@@ -1,6 +1,5 @@
-# install 2.7.13, 3.6.2, venv and venv3
-
 # '|| true' not to cause to abort when skipping install
 pyenv install 2.7.14 || true
 pyenv install 3.6.5  || true
-pyenv global 3.6.5
+pyenv virtualenv 3.6.5 venv
+pyenv global venv
