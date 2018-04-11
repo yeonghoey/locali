2b0a2e1645e86aa4b7e4a447cde7a5333dad9881
yeonghoey <yeonghoey@gmail.com>
2018-04-11 16:32:44 +0900

    Use python3 by default

--- recipes/pip-install.sh
+++ recipes/pip-install.sh
@@ -6,27 +6,6 @@ info "Use '$(command_path 'pip')'"
 
 pip install -r <(cat <<EOF
 
-ansible==2.4.0.0
-boto3==1.4.1
-botocore==1.4.78
-bpython==0.16
-click==6.7
-google-api-python-client==1.6.4
-google-cloud==0.32.0
-pydub==0.20.0
-pyperclip==1.6.0
-requests==2.18.3
-SpeechRecognition==3.8.1
-
-EOF
-)
-
-# ------------------------------------------------------------------------------
-
-info "Use '$(command_path 'pip3')'"
-
-pip3 install -r <(cat <<EOF
-
 click==6.7
 cookiecutter==1.6.0
 hew>=0.0.3
--- recipes/pyenv-install.sh
+++ recipes/pyenv-install.sh
@@ -1,8 +1,7 @@
 # install 2.7.13, 3.6.2, venv and venv3
 
 # '|| true' not to cause to abort when skipping install
-pyenv install 2.7.13 || true
-pyenv install 3.6.2  || true
-pyenv virtualenv 2.7.13 venv  || true
-pyenv virtualenv 3.6.2  venv3 || true
-pyenv global venv venv3
+pyenv install 2.7.14 || true
+pyenv install 3.6.5  || true
+pyenv virtualenv 3.6.5  venv || true
+pyenv global venv
