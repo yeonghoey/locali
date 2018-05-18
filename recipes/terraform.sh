a101f7c608d3c3b6adc8d1c415ac077265b67f2f
yeonghoey <yeonghoey@gmail.com>
2018-05-18 13:40:01 +0900

    Delete unused recipes/terraform.sh

--- recipes/terraform.sh
+++
@@ -1,9 +0,0 @@
-# install Terraform 0.10.0
-
-macos && {
-  repo_zip \
-    'https://releases.hashicorp.com/terraform/0.10.0/terraform_0.10.0_darwin_amd64.zip' \
-    'terraform'
-}
-
-repo_bin 'terraform/terraform'
