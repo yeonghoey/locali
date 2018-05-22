d4aa7963aa4b7c083c9e0c74997e111e7c7d26d1
yeonghoey <yeonghoey@gmail.com>
2018-05-22 10:50:31 +0900

    Stop using launchctl for docker-machine

--- recipes/docker.sh
+++ recipes/docker.sh
@@ -7,42 +7,11 @@ cask 'docker-toolbox'
 EOF
 )
 
-# 2. Create default docker-machine if not exists
-docker_machine_exists() {
-  docker-machine status "$1" > /dev/null 2>&1
-}
+docker-machine create --driver virtualbox default || true
 
-if ! docker_machine_exists 'default'; then
-  docker-machine create --driver virtualbox default
-fi
-
-# 3. Register the default docker-machine to run at startup
-readonly LAUNCH_AGENTS="${HOME}/Library/LaunchAgents"
-readonly PLIST_PATH="${LAUNCH_AGENTS}/com.docker.machine.default.plist"
-
-# TODO: Support macOS Startup API
-require_content "${PLIST_PATH}" << EOF
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-    <dict>
-        <key>Label</key>
-        <string>com.docker.machine.default</string>
-        <key>ProgramArguments</key>
-        <array>
-            <string>$(command_path 'docker-machine')</string>
-            <string>start</string>
-            <string>default</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-    </dict>
-</plist>
-EOF
-
-# 4. Use the docker-machine by default
 localrc 'docker' << 'EOF'
 if [[ -z "${DOCKER_HOST}" ]]; then
+  docker-machine start default || true
   eval $(docker-machine env default)
 fi
 EOF
