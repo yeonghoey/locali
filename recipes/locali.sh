abd797753e1956619cb100f34fe6dcb67dd7b616
Yeongho Kim <yeonghoey@gmail.com>
2018-05-31 21:50:52 +0900

    Remove yeonghoey.sh recipe

--- recipes/locali.sh
+++ recipes/locali.sh
@@ -1,17 +1,9 @@
-# bootstrap
+# install personal repositories
 
-symlink "${LOCALI}/locali" "${LOCAL_BIN}/locali"
+# ------------------------------------------------------------------------------
 
+info "Set url of 'origin': 'git@github.com:yeonghoey/locali.git'"
+git -C "${LOCALI}" remote set-url \
+    origin 'git@github.com:yeonghoey/locali.git'
 
-localrc 'locali' <<EOF
-
-export PATH="${LOCAL_BIN}:\${PATH}"
-
-EOF
-
-
-localrc_zsh 'locali' <<EOF
-
-fpath=("${LOCAL_COMPS_ZSH}" \$fpath)
-
-EOF
+# ------------------------------------------------------------------------------
