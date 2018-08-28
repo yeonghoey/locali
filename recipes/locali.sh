f5ff62460830ac75b1b8cdf2f796f99a1cd670dd
yeonghoey <yeonghoey@gmail.com>
2018-08-28 17:06:12 +0900

    Update locali.sh

--- recipes/locali.sh
+++ recipes/locali.sh
@@ -1,9 +1,23 @@
-# install personal repositories
+# ------------------------------------------------------------------------------
+# bootstrap
+# ------------------------------------------------------------------------------
+symlink "${LOCALI}/locali" "${LOCAL_BIN}/locali"
+localrc 'locali' <<EOF
+
+export PATH="${LOCAL_BIN}:\${PATH}"
+
+EOF
+
+
+localrc_zsh 'locali' <<EOF
+
+fpath=("${LOCAL_COMPS_ZSH}" \$fpath)
+
+EOF
 
 # ------------------------------------------------------------------------------
-
+# install personal repositories
+# ------------------------------------------------------------------------------
 info "Set url of 'origin': 'git@github.com:yeonghoey/locali.git'"
 git -C "${LOCALI}" remote set-url \
     origin 'git@github.com:yeonghoey/locali.git'
-
-# ------------------------------------------------------------------------------
