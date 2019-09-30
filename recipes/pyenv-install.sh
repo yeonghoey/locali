6d3c96d364eca6e6c93143e21d8b935d75c445db
Yeongho Kim <yeonghoey@gmail.com>
2019-09-30 21:44:00 +0900

    Use Python 3.7.4 by default

--- recipes/pyenv-install.sh
+++ recipes/pyenv-install.sh
@@ -1,7 +1,4 @@
 # install python versions
 # '|| true' not to cause to abort when skipping install
-pyenv install 2.7.15 || true
-pyenv install 3.6.5  || true
-pyenv install 3.7.0  || true
-# NOTE: Use 3.6.5 by default until 3.7.0 is stable.
-pyenv global 3.6.5 2.7.15
+pyenv install 3.7.4  || true
+pyenv global 3.7.4
